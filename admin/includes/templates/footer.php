32bbb153dcd9a0c3ddb2087d6574ac8ca659b0e8
kareemhamed001 <73015702+kareemhamed001@users.noreply.github.com>
2022-08-13 01:36:08 +0200

    Delete admin directory

--- admin/includes/templates/footer.php
+++
@@ -1,8 +0,0 @@
-       footer
-       <script src="layout/js/bootstrap.bundle.min.js"></script>
-       <script src="layout/js/bootstrap.bundle.min.js.map"></script>
-       <script src="layout/js/all.min.js"></script>
-       <script src="layout/js/backend.js"></script>
-       </body>
-
-       </html>
